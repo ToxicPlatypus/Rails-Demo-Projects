cdb1cf49f3a291c7ed92f98829d01c8167c07342
ToxicPlatypus <rafidrabbi123@gmail.com>
2023-01-24 17:47:40 +0600

    final vlog

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+  http_basic_authenticate_with name: 'rafid', password: '1234', except: [:index, :show]
+
   def index
     @articles = Article.all
   end
