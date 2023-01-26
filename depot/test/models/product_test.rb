d1758aa6cc835570564887f95925a305586e8e15
ToxicPlatypus <rafidrabbi123@gmail.com>
2023-01-26 18:49:43 +0600

    root page

--- depot/test/models/product_test.rb
+++ depot/test/models/product_test.rb
@@ -6,15 +6,15 @@ class ProductTest < ActiveSupport::TestCase
   test "product attributes must not be empty" do
     product = Product.new
     # assert product.invalid?
-    puts assert product.invalid?
+    assert product.invalid?
     # assert product.errors[:title].any?
-    puts assert product.errors[:title].any?
+    assert product.errors[:title].any?
     # assert product.errors[:description].any?
-    puts assert product.errors[:description].any?
+    assert product.errors[:description].any?
     # assert product.errors[:price].any?
-    puts assert product.errors[:price].any?
+    assert product.errors[:price].any?
     # assert product.errors[:image_url].any?
-    puts assert product.errors[:image_url].any?
+    assert product.errors[:image_url].any?
   end
 
   test "product price must be positive" do
