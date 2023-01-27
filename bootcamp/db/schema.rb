199ba8a03d940795c40911c479e0fcdda572f27b
ToxicPlatypus <rafidrabbi123@gmail.com>
2023-01-27 19:13:32 +0600

    final jan 27

--- bootcamp/db/schema.rb
+++ bootcamp/db/schema.rb
@@ -10,15 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_27_123553) do
-
-  create_table "atuhors", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "country"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 2023_01_27_122031) do
 
   create_table "students", force: :cascade do |t|
     t.string "first_name"
