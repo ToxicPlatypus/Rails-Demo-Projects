892f0f142e6659e7514c77e80689c211eddc709a
ToxicPlatypus <rafidrabbi123@gmail.com>
2023-01-30 19:10:38 +0600

    bootacamp

--- bootcamp/app/controllers/students_controller.rb
+++ bootcamp/app/controllers/students_controller.rb
@@ -16,6 +16,23 @@ class StudentsController < ApplicationController
     end
   end
 
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    if @student.update(student_params)
+      redirect_to student_path(@student)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def student_params
