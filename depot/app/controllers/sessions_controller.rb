43dc90ba14003a070babdb8c21e08697ca70cf0d
ToxicPlatypus <rafidrabbi123@gmail.com>
2023-02-09 18:27:59 +0600

    login

--- depot/app/controllers/sessions_controller.rb
+++ depot/app/controllers/sessions_controller.rb
@@ -1,11 +1,14 @@
 class SessionsController < ApplicationController
+
+  skip_before_action :authorize
+
   def new
   end
 
   def create
     user = User.find_by(name: params[:name])
     if user.try(:authenticate, params[:password])
-      session[:user_id] = user_id
+      session[:user_id] = user.id
       redirect_to admin_url
 
     else
@@ -14,5 +17,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to store_index_url, notice: 'Logged Out'
   end
 end
